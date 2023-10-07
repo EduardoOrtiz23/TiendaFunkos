d2ba03ff3d1a54c55848f5340c8897f0029a0a28
Andrick Villalobos <rude_vato@outlook.com>
2023-10-06 19:52:37 -0600

    -Se crearon procedimientos para registrar y editar usuarios. -Se crearon metodos en las clases CD_Uusuario u CN_Usuario. -Se agrego el metodo para encripat ocntraseñas con SHA256.

---
+++ CapaEntidad/CN_Recursos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class CN_Recursos
+    {
+         //encriptacion de TEXTO en SHA256
+
+        public static string ConvertirSha256(string texto)
+        {
+            StringBuilder Sb = new StringBuilder();
+            //USAR LA REFERENCIA DE "System.Security.Cryptography"
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
+
+
+    }
+}
