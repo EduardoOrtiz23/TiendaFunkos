882a98cd6ff2ee91e75ebcb88b46f48d13a0e67a
albet0 <beto.aguilera@outlook.com>
2023-10-09 21:53:17 -0600

    se agrego en el apartado NuGet (sweetalert.bootstrap) del creador Khawaja Asim

--- CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -15,6 +15,9 @@ namespace CapaPresentacionAdmin
                            "~/Scripts/fontawesome/all.min.js",
                            "~/Scripts/DataTables/jquery.dataTables.js",
                            "~/Scripts/DataTables/dataTables.responsive.js",
+                            "~/Scripts/loadingoverlay/loadingoverlay.min.js",
+                             "~/Scripts/sweetalert.min.js",
+
                            "~/Scripts/scripts.js"));
 
 
@@ -35,7 +38,9 @@ namespace CapaPresentacionAdmin
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/Site.css",
                 "~/Content/DataTables/css/jquery.dataTables.css",
-                "~/Content/DataTables/css/responsive.dataTables.css"));
+                "~/Content/DataTables/css/responsive.dataTables.css",
+                "~/Content/sweetalert.css"
+                ));
 
 
 
