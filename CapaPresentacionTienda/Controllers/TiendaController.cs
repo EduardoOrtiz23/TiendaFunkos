ee94f5c5405cedd2a7b102ed12d36b1976ae2b64
albet0 <beto.aguilera@outlook.com>
2023-11-07 23:54:44 -0600

    completado

--- CapaPresentacionTienda/Controllers/TiendaController.cs
+++ CapaPresentacionTienda/Controllers/TiendaController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CapaEntidad;
+using CapaNegocio;
+
+using System.IO;
+
 namespace CapaPresentacionTienda.Controllers
 {
     public class TiendaController : Controller
@@ -13,5 +18,46 @@ namespace CapaPresentacionTienda.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public JsonResult ListaCategorias(){
+
+            List<Categoria> lista = new List<Categoria>();
+
+            lista = new CN_Categoria().Listar();
+            return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult ListarProducto(int idcategoria)
+        {
+            List<Producto> lista = new List<Producto>();
+            bool conversion;
+
+            lista = new CN_Producto().Listar().Select(p => new Producto(){
+                IdProducto = p.idProducto,
+                Nombre = p.Nombre,
+                Descripcion = p.Descripcion,
+                oMarca = p.oMarca,
+                    oCategoria = p.oCategoria,
+                    Precio = p.Precio,
+                    Stock = p.Stock,
+                    RutaImagen = p.RutaImagen,
+                    Base64 =  CN_Recursos.ConvertirBase64(Path.Combine(p.RutaImagen,p.NombreImagen), out conversion),
+                    Extension = Path.GetExtension(p.NombreImagen),
+                    Activo = p.Activo
+
+            }).Where(p => 
+            p.oCateogria.IdCategoria == (idcategoria == 0 ? p.oCategoria.IdCategoria : idcategoria) &&
+           p.Stock > 0 && p.Activo == true
+            ).ToList();
+
+            var jsonresult = Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+            jsonresult.MaxJsonLength = int.MaxValue;
+
+            return jsonresult;
+            }
     }
+
+
 }
