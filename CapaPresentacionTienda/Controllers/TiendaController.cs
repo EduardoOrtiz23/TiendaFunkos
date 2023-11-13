bf8c552165811bfaac4fcda45cef8fa4fcc4d465
albet0 <beto.aguilera@outlook.com>
2023-11-13 01:45:48 -0600

    completado

--- CapaPresentacionTienda/Controllers/TiendaController.cs
+++ CapaPresentacionTienda/Controllers/TiendaController.cs
@@ -29,7 +29,7 @@ namespace CapaPresentacionTienda.Controllers
 
 
 
-            if(oProducto != null)
+            if (oProducto != null)
             {
                 oProducto.Base64 = CN_Recursos.ConvertirBase64(Path.Combine(oProducto.RutaImagen, oProducto.NombreImagen), out conversion);
                 oProducto.Extension = Path.GetExtension(oProducto.NombreImagen);
@@ -38,7 +38,8 @@ namespace CapaPresentacionTienda.Controllers
         }
 
         [HttpGet]
-        public JsonResult ListaCategorias(){
+        public JsonResult ListaCategorias()
+        {
 
             List<Categoria> lista = new List<Categoria>();
 
@@ -51,21 +52,23 @@ namespace CapaPresentacionTienda.Controllers
             List<Producto> lista = new List<Producto>();
             bool conversion;
 
-            lista = new CN_Producto().Listar().Select(p => new Producto(){
+            lista = new CN_Producto().Listar().Select(p => new Producto()
+            {
                 IdProducto = p.IdProducto,
                 Nombre = p.Nombre,
                 Descripcion = p.Descripcion,
-                oMarca = p.oMarca,
-                    oCategoria = p.oCategoria,
-                    Precio = p.Precio,
-                    Stock = p.Stock,
-                    RutaImagen = p.RutaImagen,
-                    Base64 =  CN_Recursos.ConvertirBase64(Path.Combine(p.RutaImagen,p.NombreImagen), out conversion),
-                    Extension = Path.GetExtension(p.NombreImagen),
-                    Activo = p.Activo
 
-            }).Where(p => 
+                oCategoria = p.oCategoria,
+                Precio = p.Precio,
+                Stock = p.Stock,
+                RutaImagen = p.RutaImagen,
+                Base64 = CN_Recursos.ConvertirBase64(Path.Combine(p.RutaImagen, p.NombreImagen), out conversion),
+                Extension = Path.GetExtension(p.NombreImagen),
+                Activo = p.Activo
+
+            }).Where(p =>
             p.oCategoria.IdCategoria == (idcategoria == 0 ? p.oCategoria.IdCategoria : idcategoria) &&
+
            p.Stock > 0 && p.Activo == true
             ).ToList();
 
@@ -73,7 +76,7 @@ namespace CapaPresentacionTienda.Controllers
             jsonresult.MaxJsonLength = int.MaxValue;
 
             return jsonresult;
-            }
+        }
     }
 
 
