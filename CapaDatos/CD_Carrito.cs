ca971457c7ed6427d417549c5c1815f8cdabf938
Lalo2023Git <lalo48383gmail.com>
2023-11-16 00:41:31 -0600

    Actualizar base de datos 2.2

--- CapaDatos/CD_Carrito.cs
+++ CapaDatos/CD_Carrito.cs
@@ -8,6 +8,7 @@ using CapaEntidad;
 
 using System.Data.SqlClient;
 using System.Data;
+using System.Globalization;
 
 namespace CapaDatos
 {
@@ -96,6 +97,81 @@ namespace CapaDatos
             catch (Exception ex)
             {
                 resultado = 0;
+            }
+            return resultado;
+        }
+
+        public List<Carrito> ListarProducto(int idcliente)
+        {
+            List<Carrito> lista = new List<Carrito>();
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "select * from fn_obtenerCarritoCliente(@idcliente)";
+
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.Parameters.AddWithValue("@idcliente", idcliente);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Carrito()
+                            {
+
+                                oProducto =  new Producto()
+                                {
+                                    IdProducto = Convert.ToInt32(dr["IdProducto"]),
+                                    Nombre = dr["Nombre"].ToString(),
+                                    oCategoria = new Categoria() { Descripcion = dr["DesCategoria"].ToString() },
+                                    Precio = Convert.ToDecimal(dr["Precio"], new CultureInfo("es-MX")),
+                                    RutaImagen = dr["RutaImagen"].ToString(),
+                                    NombreImagen = dr["NombreImagen"].ToString()
+                                },
+                                Cantidad = Convert.ToInt32(dr["Cantidad"])
+                            });
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                lista = new List<Carrito>();
+            }
+
+            return lista;
+        }
+
+        public bool EliminarCarrito(int idcliente, int idproducto)
+        {
+            bool resultado = true;
+
+            try
+            {
+
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EliminarCarrito", oconexion);
+                    cmd.Parameters.AddWithValue("IdCliente", idcliente);
+                    cmd.Parameters.AddWithValue("IdProducto", idproducto);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
             }
             return resultado;
         }
